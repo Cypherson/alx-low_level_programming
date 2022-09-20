8e88cf428a2ed21db66eb12bb9f8b8b0097d4bca
Hilary <a.aiyowie.hilary@gmail.com>
2022-09-20 13:38:44 -0700

    a function that prints every other character of a string, starting with the first character, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - a function that reverses a string.
+ * @ch: contains the reversed string
+ * @i: index string number
+ * @l: length of the string
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int l, i;
+	char ch;
+
+	for(l = 0; s[l] != '\0'; l++)
+
+		for(i = 0; i < l / 2; i++)
+		{
+			ch = s[l];
+			s[l] = s[l - 1 - i];
+			s[l - 1 -i] = ch;
+		}
+}
