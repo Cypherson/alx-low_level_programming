ab51a570482beaf5e48e970665959730761974d6
Hilary <a.aiyowie.hilary@gmail.com>
2022-09-21 12:50:23 -0700

    task 1,  a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strncat - concatinates two strings but adds an inputed number of bytes
- * @dest: destination to be concatinated upon
- * @src: string to be appended to dest
- * @n: number of bytes from src to be appended to dest
- * Returns: a pointer to the resulting string of dest
+ * _strncat - a function that concatinates two strings
+ * @dest: a buffer to be concatinated on
+ * @src: a string to be appended to dest
+ * @n: the maximum number of bytes to be copied from src
+ * Return: A pointer to the resulting string
  */
 
 char *_strncat(char *dest, char *src, int n)
@@ -13,14 +13,9 @@ char *_strncat(char *dest, char *src, int n)
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
-		src_len++;
+		dest_len++;
 
 	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
-	
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
+		dest[dest_len++] = src[index];
 
 	return (dest);
-
-}
