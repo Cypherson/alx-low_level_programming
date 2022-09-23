d7b0ca00d187fa29f47d55822e59fb2dbc5e315d
Hilary <a.aiyowie.hilary@gmail.com>
2022-09-22 20:44:09 -0700

    task 6,a function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,30 +2,36 @@
 
 /**
  * cap_string - a function that capitalizes all words of a string.
- * @s: string to be capitalised
- * Returns: always address of s
+ * @str: string to be capitalised
+ * Return: always address of s
  *
  */
 
-char *cap_string(char *)
+char *cap_string(char *str)
 {
-	int i = 0, j;
-	char a[] = " \t\n,;.!?\"(){}";
+	int index = 0;
 
-	while (*(s + i))
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
 
-		if (*(s + i(>= 'a' && *(s + i) <= 'z')
-		{
-		if (i == 0)
-			*(s + i) -= 'a' - 'A';
-		else
-		{
-			for (j = 0; j <= 12; j++)
-			{
-				if (a[j] == *(s + i - 1))
-					*(s + i) -= 'a' - 'A';
-			}
-		}
+		if (str[index - 1] == ' ' ||
+			str[index - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			 index == 0)
+			str[index] -= 32;
+		index++;
 	}
-	i++
+	return (str);
 }
