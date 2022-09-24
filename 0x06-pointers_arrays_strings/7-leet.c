0e12cb1831aaa218d2f8822eea691a5dd098b2ad
Hilary <a.aiyowie.hilary@gmail.com>
2022-09-23 21:47:21 -0700

    task 7, a function that encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,25 +2,45 @@
 
 /**
  * leet - the encoder
- * @n: input*
+ * @str: string to be encoded
  * Return: value of n
  */
 
-char *leet(char *n)
+char *leet(char *x)
 {
-	int i, j;
-	char A1[] = "aAeEoOtTlL";
-	char A2[] = "4433007711";
+	int i = 0;
 
-	for (i = 0; n[i]; != '\0'; i++)
+	while (str != '\0')
 	{
-		for (j = 0; j < 10; j++)
-		{
-			if (n[i] == A1[j])
-			{
-				n([i] A2[j]);
-			}
-		}
+		(str[i]) = transform(str[i];
+				i++;
 	}
-	return (n);
+	return (str);
+
 }
+
+/**
+ * transform - a function that maps a letter with leet code
+ * @x: char to be encoded
+ *
+ *Return: the encoded char
+ */
+
+ char transform(char x)
+ {
+ 	char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
+	char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
+	int i = 0;
+	char replacement = x;
+
+	while (i < 0)
+	{
+		if (x == mapping_low[i] || x == mapping_upper[i])
+		{
+			replacement = i + '0';
+			break;
+		}
+		i++;
+	}
+	return (replacement);
+ }
