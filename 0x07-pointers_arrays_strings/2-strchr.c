0822ed958e459d4add12e8f523ad1bf2d8b53611
Hilary <a.aiyowie.hilary@gmail.com>
2022-09-26 16:27:13 -0700

    task 2, a function that locates a character c in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,10 +10,13 @@
 
 char *_strchr(char *s, char c)
 {
-	for (i = 0; *(s + i); i++)
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
 	{
-		if (*(s + i) == c)
-			return (s + i);
+		if (s[index] == c)
+			return (s + index);
 	}
-	return (0);
+
+	return ('\0');
 }
