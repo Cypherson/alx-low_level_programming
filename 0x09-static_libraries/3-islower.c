6d91d2a725cfd758afa31bd2bf48f30dc3e8dbb2
Hilary <a.aiyowie.hilary@gmail.com>
2022-10-01 13:46:40 -0700

    the static library

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * Description: _islower.c - _islower checks for  a function that checks for lowercase character. 
+ *
+ * @c: is a single letter input
+ *
+ *Return: success
+ */
+int _islower(int c){
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * Description: _abs - computes the absolute value of an integer.
+ *
+ * @abs: input data
+ *
+ * Return: success
+ *
+ */
+int _abs(int ab)
+{
+return 
+	(ab * ((ab > 0) - (ab < 0)));
+}
