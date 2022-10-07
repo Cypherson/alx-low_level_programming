3df6aff9af971ff5be3ba3ed2d76bf9eb7e03d3d
Hilary <a.aiyowie.hilary@gmail.com>
2022-10-07 15:41:27 -0700

    task 1, a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -17,7 +17,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char ptr;
 
 	num = n;
-	if (s1 == NULL)
+
+	if (s1 = NULL)
+	if (s1 = NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
