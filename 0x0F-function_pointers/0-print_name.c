cdf6f7413332a069cfcf64821f4474c7ee9c0166
Hilary <a.aiyowie.hilary@gmail.com>
2022-10-12 16:32:40 -0700

    task 0, a function that prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * print_name - a function that prints a name.
+ * @name: is a pointer to name
+ * @f: is the function pointer
+ * Return: nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
