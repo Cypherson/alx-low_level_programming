31388b4350be1a023b6465f1e48e8b234a63c764
Hilary <a.aiyowie.hilary@gmail.com>
2022-10-21 12:44:45 -0700

    a function that prints all the elements of a list_t list.

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,28 @@
+#define _LISTS_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - is a program of a single linked list 
+ * @str: is the string
+ * @len: length of the string
+ * @next: a pointer to the next node
+ * Description: list_s is a single linked list struct.
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+ize_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif /* _LISTS_ */
