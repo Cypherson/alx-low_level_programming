20de8adc71b50a182a60509fcf68d1edf15ab455
Hilary <a.aiyowie.hilary@gmail.com>
2022-12-14 14:00:21 -0800

    task 0

--- 0x18-dynamic_libraries/4-strpbrk.c
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
  * _strpbrk - a function that searches a string for any of a set of bytes.
  * @s: string to be assessed
  * @accept: string to assess against
- *
  * Return: NULL if no match or pointer to byte in s that match
  */
 
@@ -23,11 +21,11 @@ char *_strpbrk(char *s, char *accept)
 				s += a;
 				return (s);
 			}
-			else
-				return ('\0');
+			
 			b++;
 		}
 
 		a++;
 	}
+	return ('\0');
 }
