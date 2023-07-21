7aa834ce639a637888e5e8f2c1c3844dc447cf9a
Aliu Hilary [Cypherson] <a.aiyowie.hilary@gmailo.com>
2023-07-21 16:09:57 -0700

    tried fixing again

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - tests a functions that prints
+ * if an integer is positive or negative
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	 i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
