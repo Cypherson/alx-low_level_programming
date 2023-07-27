98efb86839c980c2dae9a12333bf3a673d5899d5
Aliu Hilary [Cypherson] <a.aiyowie.hilary@gmailo.com>
2023-07-27 15:26:29 -0700

    did magic

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character 'c' to stdout
+ * @c: th character to be written out
+ * 
+ * Return: 1 on success
+ *
+ * on error, -1 is returned, and errno is set appropriately.
+ *
+ */
+
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
