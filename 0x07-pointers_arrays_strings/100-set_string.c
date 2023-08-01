ddf84b76efc1dbb1f2cb2e41f1d32a8019a78db6
Aliu Hilary [Cypherson] <a.aiyowie.hilary@gmailo.com>
2023-07-31 18:45:21 -0700

    advanced

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s: 1st pointr
+ * @to: 2nd pointer
+ *
+ * Return: void
+ *
+ * (c) Cypherson
+ *
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
