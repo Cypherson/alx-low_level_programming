9a675723344de5ad503a93071078cab9c0ebdcf6
Aliu Hilary [Cypherson] <a.aiyowie.hilary@gmailo.com>
2023-08-07 15:02:24 -0700

    tried fixing

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,16 +1,22 @@
 #include "main.h"
+
 /**
- * _puts - prints a string, followed by a new line, to stdout
- * @str: string to print
+ * _puts -  prints a string, followed by a new line, to stdout.
+ * @str: string to be printed
  *
- * Return: void
+ * Return: Always (0)
+ *
+ * (c) Cypherson
  *
  */
+
 void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		_putchar(*str++);
+		_putchar(*str + 0);
+		str++;
 	}
-		_putchar('\n');
+
+	_putchar('\n');
 }
