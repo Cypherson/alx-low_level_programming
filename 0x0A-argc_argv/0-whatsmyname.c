a43ac7fae8e22f2ff8ee66d6ee3635dfc15dee0a
Aliu Hilary [Cypherson] <a.aiyowie.hilary@gmailo.com>
2023-08-09 01:50:10 -0700

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a program that prints its name
+ * @argv: argument vector
+ * @argc: argument count
+ *
+ * Return: Always 0
+ *
+ *
+ * (c) Cyphesrson
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return (0);
+}
