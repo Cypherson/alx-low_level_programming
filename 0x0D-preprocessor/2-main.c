e7d0b8f0fb88cb5da0c99ce7469ac7883880acc3
Aliu Hilary [Cypherson] <a.aiyowie.hilary@gmailo.com>
2023-08-15 13:51:18 -0700

    fixed 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point that prints the name of the file it's compiled from.
+ *
+ * Return: Always zero
+ *
+ *
+ * (c) Cypherson
+ *
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
