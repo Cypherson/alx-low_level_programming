7f21ccc9e7c7b87c3101f612d0244952f2ea0827
Aliu Hilary [Cypherson] <a.aiyowie.hilary@gmailo.com>
2023-08-31 15:09:40 -0700

    mandatory

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index.
+ * @n: pointer to bit
+ * @index: is the index, starting from 0 of the bit to set
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ *
+ *
+ * (c) Cypherson
+ *
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int val;
+
+	if (index >= 64)
+		return (-1);
+
+	val = 1 << index;
+	*n = (*n | val);
+
+	return (1);
+}
